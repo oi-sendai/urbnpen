88056ca89cfa8db5528bf783d93140a2a91870f8
Andrew MacKay <croftnotworking@gmail.com>
2014-09-14 06:42:57 +0300

    collections page completed

--- client/js/gallery/galleryController.js
+++ client/js/gallery/galleryController.js
@@ -13,15 +13,40 @@ galleryController.controller('galleryController', function(
   	$scope.works = 'js/gallery/galleryController';
     $rootScope.class ="gallery";
 
-	$scope.slides = [
-	    // {image: '/images/ioana/p1.jpg', description: 'Image 00'},
-	    {image: '/images/ioana/p2.jpg', description: 'Image 01'},
-	    {image: '/images/ioana/p3.jpg', description: 'Image 02'},
-	    {image: '/images/ioana/p4.jpg', description: 'Image 03'},
-	    {image: '/images/ioana/p5.jpg', description: 'Image 04'}
-	];
+	$scope.collections = [
 
- 
+{
+    "slides": [
+        {image: '/images/slides/h1.jpg', description: 'Image 00'},
+        {image: '/images/slides/h2.jpg', description: 'Image 01'},
+        {image: '/images/slides/h3.jpg', description: 'Image 02'},
+        {image: '/images/slides/h4.jpg', description: 'Image 03'},
+        {image: '/images/slides/h5.jpg', description: 'Image 04'}
+    ],
+    "description": "Summer Collection"
+},
+{
+    "slides": [
+        {image: '/images/slides/p1.jpg', description: 'Image 00'},
+        {image: '/images/slides/p2.jpg', description: 'Image 01'},
+        {image: '/images/slides/p3.jpg', description: 'Image 02'},
+        {image: '/images/slides/p4.jpg', description: 'Image 03'},
+        {image: '/images/slides/p5.jpg', description: 'Image 04'}
+    ],
+    "description": "Autumn Collection"
+},
+{
+    "slides": [
+        {image: '/images/slides/e1.jpg', description: 'Image 00'},
+        {image: '/images/slides/e2.jpg', description: 'Image 01'},
+        {image: '/images/slides/e3.jpg', description: 'Image 02'},
+        {image: '/images/slides/e4.jpg', description: 'Image 03'},
+        {image: '/images/slides/e5.jpg', description: 'Image 04'}
+    ],
+    "description": "Spring Collection"
+},
+
+];
 
 
     $scope.open = function(slide){
